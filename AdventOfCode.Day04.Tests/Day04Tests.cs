f20886321ae5a5599037b041a5c375eb8b8768b2
Kevin Hunter <kevin.hunter@sylvania.com>
2015-12-04 10:56:28 -0500

    part 1 done

---
+++ AdventOfCode.Day04.Tests/Day04Tests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace AdventOfCode.Day04.Tests
+{
+    [TestFixture]
+    public class Day04Tests
+    {
+        [TestCase("abcdef", 609043)]
+        [TestCase("pqrstuv", 1048970)]
+        public void GetLowestConcatNumberForSeed(string seed, int expectedNumber)
+        {
+            var result = Processor.GetLowestConcatNumberForSeed(seed);
+            Assert.AreEqual(expectedNumber, result);
+        }
+    }
+}
--- AdventOfCode.Day04/Program.cs
+++ AdventOfCode.Day04/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,39 @@ namespace AdventOfCode.Day04
     {
         static void Main(string[] args)
         {
+            var part1Answer = Processor.GetLowestConcatNumberForSeed("yzbqklnj");
+        }
+    }
+
+    public static class Processor
+    {
+        public static int GetLowestConcatNumberForSeed(string seed)
+        {
+            //hard limit ourselves at 4 million
+            var suffixNumbers = Enumerable.Range(1, 4000000);
+
+            var lowestWorkingNumber = suffixNumbers
+                .Where(x => CheckHash(seed, x))
+                .First();
+
+            return lowestWorkingNumber;
+        }
+
+        private static bool CheckHash(string seed, int suffix)
+        {
+            using (var hasher = MD5.Create())
+            {
+                var hashBytes = hasher.ComputeHash(Encoding.UTF8.GetBytes(seed + suffix));
+
+                var builder = new StringBuilder();
+                foreach (var hashByte in hashBytes)
+                {
+                    builder.Append(hashByte.ToString("x2"));
+                }
+
+                var fullHash = builder.ToString();
+                return fullHash.Substring(0, 5) == "00000";
+            }
         }
     }
 }
