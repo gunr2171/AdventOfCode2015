3588ec174bbde5e9504b65c4e763ebd42742f29a
gunr2171 <kevin@kevin-hunter.net>
2015-12-05 00:37:04 -0500

    day 5 part 1 done

---
+++ AdventOfCode.Day05.Tests/Day05Tests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace AdventOfCode.Day05.Tests
+{
+    [TestFixture]
+    public class Day05Tests
+    {
+        [TestCase("ugknbfddgicrmopn", true)]
+        [TestCase("aaa", true)]
+        [TestCase("jchzalrnumimnmhp", false)]
+        [TestCase("haegwjzuvuyypxyu", false)]
+        [TestCase("dvszwmarrgswjxmb", false)]
+        public void IsNiceString(string input, bool expectedNiceValue)
+        {
+            var result = Processor.IsNiceString(input);
+            Assert.AreEqual(expectedNiceValue, result);
+        }
+    }
+}
---
+++ AdventOfCode.Day05/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Day05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var part1Answer = File.ReadAllLines("Input.txt")
+                .Where(x => Processor.IsNiceString(x))
+                .Count();
+        }
+    }
+
+    public static class Processor
+    {
+        static char[] vowels = new char[] { 'a', 'e', 'i', 'o', 'u' };
+        static string[] badPhrases = new string[] { "ab", "cd", "pq", "xy" };
+
+        public static bool IsNiceString(string input)
+        {
+            var vowelCount = (from inputChar in input
+                              join vowelChar in vowels on inputChar equals vowelChar
+                              select inputChar)
+                              .Count();
+
+            if (vowelCount < 3)
+                return false;
+
+            var hasRepeatingCharacter = input
+                .Skip(1)
+                .Where((x, i) => x == input[i])
+                .Any();
+
+            if (!hasRepeatingCharacter)
+                return false;
+
+            var firstFoundBadPhrase = badPhrases
+                .Where(x => input.Contains(x))
+                .FirstOrDefault();
+
+            if (firstFoundBadPhrase != null)
+                return false;
+
+            //didn't catch on any of the above rules, it's good
+            return true;
+        }
+    }
+}
