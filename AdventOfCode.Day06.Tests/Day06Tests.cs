4013d399896b6f3685bb360720f101d468af1244
gunr2171 <kevin@kevin-hunter.net>
2015-12-06 00:05:44 -0500

    rename tests file

--- AdventOfCode.Day06.Tests/Day06Tests.cs
+++ AdventOfCode.Day06.Tests/Day06Tests.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace AdventOfCode.Day06.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class Day06Tests
     {
         [TestMethod]
         public void TestMethod1()
