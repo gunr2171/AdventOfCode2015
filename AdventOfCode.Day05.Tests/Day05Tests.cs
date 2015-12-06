392b60f33a2b1b44f2e00d3030fc5eb662a44345
gunr2171 <kevin@kevin-hunter.net>
2015-12-05 23:56:36 -0500

    remove some commented code

--- AdventOfCode.Day05.Tests/Day05Tests.cs
+++ AdventOfCode.Day05.Tests/Day05Tests.cs
@@ -58,31 +58,5 @@ namespace AdventOfCode.Day05.Tests
             var result = Processor.DetermineIfInputHasValidRepeatingLetterPair(input);
             Assert.AreEqual(expectedResult, result);
         }
-
-        //[TestCase("ab", "bc", -1)]
-        //public void LetterPairCompare(string s1, string s2, int expectedCompare)
-        //{
-        //    var pair1 = new Processor.LetterPair(s1[0], s1[1], 0);
-        //    var pair2 = new Processor.LetterPair(s2[0], s2[1], 0);
-
-        //    var result = pair1.CompareTo(pair2);
-
-        //    Assert.AreEqual(expectedCompare, result);
-        //}
-
-        //[Test]
-        //public void TestComboGenerator()
-        //{
-        //    var sourceList = new List<Processor.LetterPair>();
-        //    sourceList.Add(new Processor.LetterPair('a', 'b', 0));
-        //    sourceList.Add(new Processor.LetterPair('a', 'b', 8));
-        //    //sourceList.Add(new Processor.LetterPair('a', 'b', 12));
-
-        //    var compare = sourceList[0].CompareTo(sourceList[1]);
-
-        //    var combinations = sourceList.GetKCombs(2).ToList();
-
-        //    Assert.AreEqual(1, combinations.Count);
-        //}
     }
 }
