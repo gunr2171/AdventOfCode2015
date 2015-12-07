a1b83d4c4a14d8996f75c9d9d7139377454a154d
gunr2171 <kevin@kevin-hunter.net>
2015-12-07 01:27:57 -0500

    update nuget packages and start of day 07

---
+++ AdventOfCode.Day07.Tests/Day07Tests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace AdventOfCode.Day07.Tests
+{
+    [TestFixture]
+    public class Day07Tests
+    {
+        //[TestCase()]
+        //public void TestMethod1()
+        //{
+        //}
+    }
+}
