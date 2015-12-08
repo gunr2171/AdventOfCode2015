cfc39222defa4f81dea01d1e3ad552b799b46bd3
Kevin Hunter <kevin.hunter@sylvania.com>
2015-12-08 10:34:51 -0500

    almost got part 1

--- AdventOfCode.Day08/Program.cs
+++ AdventOfCode.Day08/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace AdventOfCode.Day08
@@ -12,6 +13,16 @@ namespace AdventOfCode.Day08
         static void Main(string[] args)
         {
             var entries = File.ReadAllLines("Input.txt").ToList();
+
+            var literalCharCount = entries
+                .Select(x => Processor.GetLiteralCharacterCount(x))
+                .Sum();
+
+            var memoryCharCount = entries
+                .Select(x => Processor.GetMemoryCharacterCount(x))
+                .Sum();
+
+            var part1Answer = literalCharCount - memoryCharCount;
         }
     }
 
@@ -22,9 +33,14 @@ namespace AdventOfCode.Day08
             return input.Length;
         }
 
-        public static int GetMemoryCharacterCount(string intput)
+        public static int GetMemoryCharacterCount(string input)
         {
-            throw new NotImplementedException();
+            input = Regex.Replace(input, @"(\\x\w\w)", "_"); //get rid of hex values
+            input = Regex.Replace(input, @"(\\.)", "_"); //get rid of a slash-something.
+            input = input.Substring(1, input.Length - 2); //get rid of the first and last char (double quotes)
+
+            return input.Length;
         }
+
     }
 }
