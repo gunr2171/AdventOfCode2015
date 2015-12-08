36e56d393389243a054bbc780159587942977a76
Kevin Hunter <kevin.hunter@sylvania.com>
2015-12-08 09:17:05 -0500

    empty day 08 code

---
+++ AdventOfCode.Day08.Tests/Day08Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace AdventOfCode.Day08.Tests
+{
+    [TestFixture]
+    public class Day08Tests
+    {
+        [Test]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
---
+++ AdventOfCode.Day08/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Day08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    public static class Processor
+    {
+        public static int GetLiteralCharacterCount(string intput)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int GetEscapedCharacterCount(string intput)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
