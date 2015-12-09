74025d02d96c7bbc2cfa88387e7c2efcf4595cac
Kevin Hunter <kevin.hunter@sylvania.com>
2015-12-09 09:39:53 -0500

    part 1 example works

--- AdventOfCode.Day09.Tests/Day09Tests.cs
+++ AdventOfCode.Day09.Tests/Day09Tests.cs
@@ -7,8 +7,17 @@ namespace AdventOfCode.Day09.Tests
     public class Day09Tests
     {
         [Test]
-        public void TestMethod1()
+        public void Part1Example()
         {
+            var calculator = new Calculator();
+
+            calculator.AddDistanceRecord("London to Dublin = 464");
+            calculator.AddDistanceRecord("London to Belfast = 518");
+            calculator.AddDistanceRecord("Dublin to Belfast = 141");
+
+            var actual = calculator.CalculateShortestRoute();
+
+            Assert.AreEqual(605, actual);
         }
     }
 }
--- AdventOfCode.Day09/Program.cs
+++ AdventOfCode.Day09/Program.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using TCL.Extensions;
 
 namespace AdventOfCode.Day09
 {
@@ -10,6 +12,111 @@ namespace AdventOfCode.Day09
     {
         static void Main(string[] args)
         {
+        }
+    }
+
+    public class Calculator
+    {
+        private List<CityDistance> CityDistances;
+
+        public Calculator()
+        {
+            CityDistances = new List<CityDistance>();
+        }
+
+        public void AddDistanceRecord(string distanceRecord)
+        {
+            CityDistances.Add(new CityDistance(distanceRecord));
+        }
+
+        public int CalculateShortestRoute()
+        {
+            var cityCount = EnumCities().Count();
+            var allCityCombinations = EnumCities().GetKCombs(cityCount);
+
+            var shortestDistance = allCityCombinations
+                .Select(x => GenerateDistanceFromCityList(x))
+                .Min();
+
+            return shortestDistance;
+        }
+
+        private int GenerateDistanceFromCityList(IEnumerable<string> cityList)
+        {
+            var list = cityList.ToList();
+            var runningSum = 0;
+
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                var distanceRecord = CityDistances
+                    .Where(x => x.Cities.Contains(list[i]))
+                    .Where(x => x.Cities.Contains(list[i + 1]))
+                    .Single();
+
+                runningSum += distanceRecord.Distance;
+            }
+
+            return runningSum;
+        }
+
+        private IEnumerable<string> EnumCities()
+        {
+            return CityDistances
+                .SelectMany(x => x.Cities)
+                .Distinct();
+        }
+    }
+
+    public class CityDistance
+    {
+        public List<string> Cities { get; set; }
+        public int Distance { get; set; }
+
+        public CityDistance(string rawDistanceRecord)
+        {
+            Cities = new List<string>();
+
+            var match = Regex.Match(rawDistanceRecord, @"^(\w+) to (\w+) = (\d+)$");
+
+            Cities.Add(match.Groups[1].Value);
+            Cities.Add(match.Groups[2].Value);
+
+            Distance = match.Groups[3].Value.Parse<int>();
+        }
+    }
+
+    public static class Extensions
+    {
+        public static IEnumerable<IEnumerable<T>> GetPermutations<T>(this IEnumerable<T> list, int length)
+        {
+            if (length == 1) return list.Select(t => new T[] { t });
+            return GetPermutations(list, length - 1)
+                .SelectMany(t => list.Where(o => !t.Contains(o)),
+                    (t1, t2) => t1.Concat(new T[] { t2 }));
+        }
+
+        public static IEnumerable<IEnumerable<T>> GetPermutationsWithRept<T>(this IEnumerable<T> list, int length)
+        {
+            if (length == 1) return list.Select(t => new T[] { t });
+            return GetPermutationsWithRept(list, length - 1)
+                .SelectMany(t => list,
+                    (t1, t2) => t1.Concat(new T[] { t2 }));
+        }
+
+        public static IEnumerable<IEnumerable<T>> GetKCombsWithRept<T>(this IEnumerable<T> list, int length) where T : IComparable
+        {
+            if (length == 1) return list.Select(t => new T[] { t });
+            return GetKCombsWithRept(list, length - 1)
+                .SelectMany(t => list.Where(o => o.CompareTo(t.Last()) >= 0),
+                    (t1, t2) => t1.Concat(new T[] { t2 }));
+        }
+
+        public static IEnumerable<IEnumerable<T>> GetKCombs<T>(this IEnumerable<T> list, int length) where T : IComparable
+        {
+            if (length == 1) return list.Select(t => new T[] { t });
+            return GetKCombs(list, length - 1)
+                .SelectMany(t => list.Where(o => o.CompareTo(t.Last()) > 0),
+                    (t1, t2) => t1.Concat(new T[] { t2 }));
         }
     }
 }
