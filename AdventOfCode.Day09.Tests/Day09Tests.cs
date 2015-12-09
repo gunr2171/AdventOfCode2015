d9b3bb41777fb196bb8aad8abdd78b0318fe1b7e
Kevin Hunter <kevin.hunter@sylvania.com>
2015-12-09 09:08:34 -0500

    adding empty day 09 code

---
+++ AdventOfCode.Day09.Tests/Day09Tests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace AdventOfCode.Day09.Tests
+{
+    [TestFixture]
+    public class Day09Tests
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
