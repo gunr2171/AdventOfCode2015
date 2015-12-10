66d41e1e8e0b4176620e6474027152f772e9c10c
Kevin Hunter <kevin.hunter@sylvania.com>
2015-12-10 08:52:42 -0500

    empty day 10

---
+++ AdventOfCode.Day10.Tests/Day10Tests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace AdventOfCode.Day10.Tests
+{
+    [TestFixture]
+    public class Day10Tests
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
