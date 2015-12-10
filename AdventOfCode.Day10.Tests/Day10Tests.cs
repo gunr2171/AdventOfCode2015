16b0e31ca6f9f69fa6c6ac73005b834250be7289
Kevin Hunter <kevin.hunter@sylvania.com>
2015-12-10 09:09:41 -0500

    first attempt at part 1 I'm "posting too much data", so I don't think this is right (the answer is too long)

--- AdventOfCode.Day10.Tests/Day10Tests.cs
+++ AdventOfCode.Day10.Tests/Day10Tests.cs
@@ -6,9 +6,15 @@ namespace AdventOfCode.Day10.Tests
     [TestFixture]
     public class Day10Tests
     {
-        [Test]
-        public void TestMethod1()
+        [TestCase("1", "11")]
+        [TestCase("11", "21")]
+        [TestCase("21", "1211")]
+        [TestCase("1211", "111221")]
+        [TestCase("111221", "312211")]
+        public void GetNextLookAndSayValue(string intput, string expectedNextValue)
         {
+            var actual = Processor.GetNextLookAndSayValue(intput);
+            Assert.AreEqual(expectedNextValue, actual);
         }
     }
 }
--- AdventOfCode.Day10/Program.cs
+++ AdventOfCode.Day10/Program.cs
@@ -10,6 +10,52 @@ namespace AdventOfCode.Day10
     {
         static void Main(string[] args)
         {
+            var input = "1321131112";
+
+            var part1TempAnswer = input;
+
+            for (int i = 0; i < 40; i++)
+            {
+                part1TempAnswer = Processor.GetNextLookAndSayValue(part1TempAnswer);
+            }
+
+            var part1Answer = part1TempAnswer;
+        }
+    }
+
+    public static class Processor
+    {
+        public static string GetNextLookAndSayValue(string input)
+        {
+            var builder = new StringBuilder();
+
+            var currentRunChar = input[0];
+            var currentRunCount = 0;
+
+            foreach (var currentLetter in input)
+            {
+                if (currentLetter == currentRunChar)
+                {
+                    currentRunCount++;
+                }
+                else
+                {
+                    //dump the info about the last run
+                    builder.Append(currentRunCount);
+                    builder.Append(currentRunChar);
+
+                    //start a new run
+                    currentRunChar = currentLetter;
+                    currentRunCount = 1;
+                }
+            }
+
+            //done with the loop, dump the info on the last group
+            builder.Append(currentRunCount);
+            builder.Append(currentRunChar);
+
+            var result = builder.ToString();
+            return result;
         }
     }
 }
