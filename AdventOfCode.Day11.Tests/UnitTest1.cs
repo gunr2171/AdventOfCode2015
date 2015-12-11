7ab1e89cf2cbc744be306e03f700b06b63bb5d92
Kevin Hunter <kevin.hunter@sylvania.com>
2015-12-11 09:30:49 -0500

    empty day 11

---
+++ AdventOfCode.Day11.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace AdventOfCode.Day11.Tests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
