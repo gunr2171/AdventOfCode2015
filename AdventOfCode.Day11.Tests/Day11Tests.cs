80bf3c3bb5b524c281271d389c01f81f35857223
Kevin Hunter <kevin.hunter@sylvania.com>
2015-12-11 10:01:38 -0500

    got the 3 rules working

--- AdventOfCode.Day11.Tests/Day11Tests.cs
+++ AdventOfCode.Day11.Tests/Day11Tests.cs
@@ -6,10 +6,41 @@ namespace AdventOfCode.Day11.Tests
     [TestFixture]
     public class Day11Tests
     {
-        [Test]
-        public void PassesRule1()
+        [TestCase("hijklmmn", true)]
+        [TestCase("abbceffg", false)]
+        [TestCase("abc", true)]
+        public void PassesRule1(string input, bool expectedPass)
         {
+            var actual = Processor.PassesRule1(input);
+            Assert.AreEqual(expectedPass, actual);
+        }
 
+        [TestCase("hijklmmn", false)]
+        [TestCase("abc", true)]
+        [TestCase("abi", false)]
+        public void PassesRule2(string input, bool expectedPass)
+        {
+            var actual = Processor.PassesRule2(input);
+            Assert.AreEqual(expectedPass, actual);
+        }
+
+        [TestCase("abbceffg", true)]
+        [TestCase("abbcegjk", false)]
+        [TestCase("aabb", true)]
+        [TestCase("aaa", false)]
+        [TestCase("aaaa", true)]
+        public void PassesRule3(string input, bool expectedPass)
+        {
+            var actual = Processor.PassesRule3(input);
+            Assert.AreEqual(expectedPass, actual);
+        }
+
+        [TestCase("abcdefgh", "abcdffaa")]
+        [TestCase("ghijklmn", "ghjaabcc")]
+        public void GetNextPassword(string input, string expectedNextPassword)
+        {
+            var actual = Processor.GetNextPassword(input);
+            Assert.AreEqual(expectedNextPassword, actual);
         }
     }
 }
--- AdventOfCode.Day11/Program.cs
+++ AdventOfCode.Day11/Program.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using TCL.Extensions;
 
 namespace AdventOfCode.Day11
 {
@@ -15,19 +17,69 @@ namespace AdventOfCode.Day11
 
     public static class Processor
     {
-        public bool PassesRule1(string input)
-        {
+        private static List<char> passwordLetters;
 
+        static Processor() //yeah static constructors, because it makes sense!
+        {
+            passwordLetters = Enumerable.Range(97, 26)
+                .Select(x => (char)x)
+                .OrderBy(x => x)
+                .ToList();
         }
 
-        public bool PassesRule2(string input)
+        public static string GetNextPassword(string input)
         {
-
+            throw new NotImplementedException();
         }
 
-        public bool PassesRule3(string input)
+        public static bool PassesRule1(string input)
         {
+            //not that I expect this to happen, but just in case
+            if (input.Length < 3)
+                return false;
 
+            var incrementingLetterCount = 1;
+
+            for (int i = 1; i < input.Length; i++)
+            {
+                var delta = input[i] - input[i - 1];
+
+                if (delta == 1)
+                {
+                    incrementingLetterCount++;
+
+                    //have we reached a full set?
+                    if (incrementingLetterCount == 3)
+                    {
+                        return true;
+                    }
+                }
+                else
+                {
+                    //reset
+                    incrementingLetterCount = 1;
+                }
+            }
+
+            //never hit 3
+            return false;
+        }
+
+        public static bool PassesRule2(string input)
+        {
+            var invalidLetters = new[] { 'i', 'o', 'l' };
+
+            var containsInvalidLetter = input
+                .Where(x => x.In(invalidLetters))
+                .Any();
+
+            return !containsInvalidLetter;
+        }
+
+        public static bool PassesRule3(string input)
+        {
+            var isMatch = Regex.IsMatch(input, @"(.)\1.*(.)\2");
+            return isMatch;
         }
     }
 }
