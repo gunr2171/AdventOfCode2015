9af4b256de4af432d703186b768f2580d303d866
gunr2171 <kevin@kevin-hunter.net>
2015-12-13 00:07:42 -0500

    done with part 2 not the best method, but it works

--- AdventOfCode.Day12.Tests/Day12Tests.cs
+++ AdventOfCode.Day12.Tests/Day12Tests.cs
@@ -19,5 +19,16 @@ namespace AdventOfCode.Day12.Tests
             var actualSum = Processor.GetSumOfAllNumbersInJson(inputJson);
             Assert.AreEqual(expectedNumberSum, actualSum);
         }
+
+
+        [TestCase("[1,2,3]", 6)]
+        [TestCase("[1,{\"c\":\"red\",\"b\":2},3]", 4)]
+        [TestCase("{\"d\":\"red\",\"e\":[1,2,3,4],\"f\":5}", 0)]
+        [TestCase("[1,\"red\",5]", 6)]
+        public void GetSumOfAllNonRedNumbers(string inputJson, int expectedNumberSum)
+        {
+            var actualSum = Processor.GetSumOfAllNonRedNumbers(inputJson);
+            Assert.AreEqual(expectedNumberSum, actualSum);
+        }
     }
 }
--- AdventOfCode.Day12/Program.cs
+++ AdventOfCode.Day12/Program.cs
@@ -14,13 +14,26 @@ namespace AdventOfCode.Day12
         {
             var inputContent = File.ReadAllText("Input.txt");
 
-            var part1Answer = Processor.GetSumOfAllNumbersInJson(inputContent);            
+            var part1Answer = Processor.GetSumOfAllNumbersInJson(inputContent);
+            var part2Answer = Processor.GetSumOfAllNonRedNumbers(inputContent);
         }
     }
 
     public static class Processor
     {
         public static int GetSumOfAllNumbersInJson(string input)
+        {
+            var json = ParseJsonString(input);
+
+            var sum = json.Descendants()
+                .Where(x => x.Type == JTokenType.Integer)
+                .Select(x => x.Value<int>())
+                .Sum();
+
+            return sum;
+        }
+
+        private static JContainer ParseJsonString(string input)
         {
             JContainer json;
 
@@ -29,12 +42,51 @@ namespace AdventOfCode.Day12
             else //starts with "[", so it's an array
                 json = JArray.Parse(input);
 
-            var sum = json.Descendants()
+            return json;
+        }
+
+        public static int GetSumOfAllNonRedNumbers(string input)
+        {
+            var json = ParseJsonString(input);
+
+            var nonRedSum = json
+                .Descendants()
                 .Where(x => x.Type == JTokenType.Integer)
-                .Select(x => x.Value<int>())
+                .Select(x => GetValueOfIntegerNode(x))
                 .Sum();
 
-            return sum;
+            return nonRedSum;
+        }
+
+        private static int GetValueOfIntegerNode(JToken node)
+        {
+            //we need to look up the node's ancestors
+            var objectAncestors = node.Ancestors()
+                .Where(x => x.Type == JTokenType.Object);
+
+            //check through each of them to see if they contain a "red"
+            foreach (var oAncestor in objectAncestors)
+            {
+                var valuesInObject = oAncestor
+                    .Select(x => x as JProperty)
+                    .Select(x => x.Value);
+
+                var anyRedValues = valuesInObject
+                    .Where(x => x.Type == JTokenType.String)
+                    .Where(x => x.Value<string>() == "red")
+                    .Any();
+
+                if (anyRedValues)
+                {
+                    //one of the ancestors to this node has a red value,
+                    //we can't use this one
+                    return 0;
+                }
+            }
+
+            //checked all the ancestors to this value, none have "red"
+            //return the value
+            return node.Value<int>();
         }
     }
 }
