d8af20027361f0c030d571e4d90ac739445f10fe
gunr2171 <kevin@kevin-hunter.net>
2015-12-13 00:38:09 -0500

    got example for part 1 working

--- AdventOfCode.Day13.Tests/Day13Tests.cs
+++ AdventOfCode.Day13.Tests/Day13Tests.cs
@@ -7,8 +7,35 @@ namespace AdventOfCode.Day13.Tests
     public class Day13Tests
     {
         [Test]
-        public void TestMethod1()
+        public void Part1Example()
         {
+            var relationships = new[]
+            {
+                "Alice would gain 54 happiness units by sitting next to Bob.",
+                "Alice would lose 79 happiness units by sitting next to Carol.",
+                "Alice would lose 2 happiness units by sitting next to David.",
+                "Bob would gain 83 happiness units by sitting next to Alice.",
+                "Bob would lose 7 happiness units by sitting next to Carol.",
+                "Bob would lose 63 happiness units by sitting next to David.",
+                "Carol would lose 62 happiness units by sitting next to Alice.",
+                "Carol would gain 60 happiness units by sitting next to Bob.",
+                "Carol would gain 55 happiness units by sitting next to David.",
+                "David would gain 46 happiness units by sitting next to Alice.",
+                "David would lose 7 happiness units by sitting next to Bob.",
+                "David would gain 41 happiness units by sitting next to Carol."
+            };
+
+            var table = new Table();
+
+            foreach (var relationship in relationships)
+            {
+                table.AddRelationship(relationship);
+            }
+
+            var expectedBestNetHappiness = 330;
+            var actual = table.FindBestNetHappinessArrangement();
+
+            Assert.AreEqual(expectedBestNetHappiness, actual);
         }
     }
 }
--- AdventOfCode.Day13/Program.cs
+++ AdventOfCode.Day13/Program.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using TCL.Extensions;
 
 namespace AdventOfCode.Day13
 {
@@ -10,6 +12,103 @@ namespace AdventOfCode.Day13
     {
         static void Main(string[] args)
         {
+        }
+    }
+
+    public class Table
+    {
+        private List<Relationship> relationships = new List<Relationship>();
+
+        public void AddRelationship(string rawRelationship)
+        {
+            relationships.Add(new Relationship(rawRelationship));
+        }
+
+        public int FindBestNetHappinessArrangement()
+        {
+            var allTableMembers = relationships
+                .Select(x => x.AffectedPerson)
+                .Distinct()
+                .ToList();
+
+            var allArrangements = allTableMembers.GetPermutations(allTableMembers.Count);
+
+            var bestNetHappiness = allArrangements
+                .Select(x => DetermineNetHappinessForArangement(x.ToList()))
+                .Max();
+
+            return bestNetHappiness;
+        }
+
+        private int DetermineNetHappinessForArangement(List<string> members)
+        {
+            var netHappiness = 0;
+
+            for (int i = 0; i < members.Count - 1; i++) //go through all members (except for the last one)
+            {
+                //compare this affected by the next
+                netHappiness += GetHappinessOffset(members[i], members[i + 1]);
+
+                //compare the next affected by this
+                netHappiness += GetHappinessOffset(members[i + 1], members[i]);
+            }
+
+            //compare the first affected by the last
+            netHappiness += GetHappinessOffset(members.First(), members.Last());
+
+            //compare the last affected by the first
+            netHappiness += GetHappinessOffset(members.Last(), members.First());
+
+            return netHappiness;
+        }
+
+        private int GetHappinessOffset(string affectedPerson, string influencer)
+        {
+            var relationship = relationships
+                .Where(x => x.AffectedPerson == affectedPerson)
+                .Where(x => x.Influencer == influencer)
+                .Single();
+
+            return relationship.HappinessOffset;
+        }
+    }
+
+    public class Relationship
+    {
+        /// <summary>
+        /// The person who's happiness will be modified by relationship
+        /// </summary>
+        public string AffectedPerson { get; set; }
+
+        /// <summary>
+        /// The person who will be affecting the Affected Person.
+        /// </summary>
+        public string Influencer { get; set; }
+
+        public int HappinessOffset { get; set; }
+
+        public Relationship(string rawRelationship)
+        {
+            var match = Regex.Match(rawRelationship, @"^(\w+) would (gain|lose) (\d+) happiness units by sitting next to (\w+).$");
+
+            AffectedPerson = match.Groups[1].Value;
+            Influencer = match.Groups[4].Value;
+
+            HappinessOffset = match.Groups[3].Value.Parse<int>();
+
+            if (match.Groups[2].Value == "lose")
+                HappinessOffset = -HappinessOffset; //negitize the value
+        }
+    }
+
+    public static class Extensions
+    {
+        public static IEnumerable<IEnumerable<T>> GetPermutations<T>(this IEnumerable<T> list, int length)
+        {
+            if (length == 1) return list.Select(t => new T[] { t });
+            return GetPermutations(list, length - 1)
+                .SelectMany(t => list.Where(o => !t.Contains(o)),
+                    (t1, t2) => t1.Concat(new T[] { t2 }));
         }
     }
 }
