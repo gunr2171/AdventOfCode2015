be4cdf3c2b8f76171755064e21509505d77cf6de
gunr2171 <kevin@kevin-hunter.net>
2015-12-13 00:10:33 -0500

    empty day 13 code

---
+++ AdventOfCode.Day13.Tests/Day13Tests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace AdventOfCode.Day13.Tests
+{
+    [TestFixture]
+    public class Day13Tests
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
