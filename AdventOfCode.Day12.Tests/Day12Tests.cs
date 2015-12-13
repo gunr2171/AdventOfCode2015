dc3adb5b9242c96ae5b85726c2256fca6f86c794
gunr2171 <kevin@kevin-hunter.net>
2015-12-12 23:24:46 -0500

    done with part 1

--- AdventOfCode.Day12.Tests/Day12Tests.cs
+++ AdventOfCode.Day12.Tests/Day12Tests.cs
@@ -6,9 +6,18 @@ namespace AdventOfCode.Day12.Tests
     [TestFixture]
     public class Day12Tests
     {
-        [Test]
-        public void TestMethod1()
+        [TestCase("[1,2,3]", 6)]
+        [TestCase("{\"a\":2,\"b\":4}", 6)]
+        [TestCase("[[[3]]]", 3)]
+        [TestCase("{\"a\":{\"b\":4},\"c\":-1}", 3)]
+        [TestCase("{\"a\":[-1,1]}", 0)]
+        [TestCase("[-1,{\"a\":1}]", 0)]
+        [TestCase("[]", 0)]
+        [TestCase("{}", 0)]
+        public void GetSumOfAllNumberInJson(string inputJson, int expectedNumberSum)
         {
+            var actualSum = Processor.GetSumOfAllNumbersInJson(inputJson);
+            Assert.AreEqual(expectedNumberSum, actualSum);
         }
     }
 }
--- AdventOfCode.Day12/Program.cs
+++ AdventOfCode.Day12/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,29 @@ namespace AdventOfCode.Day12
     {
         static void Main(string[] args)
         {
+            var inputContent = File.ReadAllText("Input.txt");
+
+            var part1Answer = Processor.GetSumOfAllNumbersInJson(inputContent);            
+        }
+    }
+
+    public static class Processor
+    {
+        public static int GetSumOfAllNumbersInJson(string input)
+        {
+            JContainer json;
+
+            if (input.StartsWith("{")) //this is an object
+                json = JObject.Parse(input);
+            else //starts with "[", so it's an array
+                json = JArray.Parse(input);
+
+            var sum = json.Descendants()
+                .Where(x => x.Type == JTokenType.Integer)
+                .Select(x => x.Value<int>())
+                .Sum();
+
+            return sum;
         }
     }
 }
