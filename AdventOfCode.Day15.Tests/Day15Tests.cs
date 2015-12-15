830622b4a1dc06b86bcc14a00f7445561c36aeff
Kevin Hunter <kevin.hunter@sylvania.com>
2015-12-15 10:39:26 -0500

    part of the example done for part 1

--- AdventOfCode.Day15.Tests/Day15Tests.cs
+++ AdventOfCode.Day15.Tests/Day15Tests.cs
@@ -1,15 +1,34 @@
 ﻿using System;
 using NUnit.Framework;
+using System.Collections.Generic;
 
 namespace AdventOfCode.Day15.Tests
 {
     [TestFixture]
     public class Day15Tests
     {
-        [Test]
-        public void TestMethod1()
-        {
+        Recipe recipe;
 
+        [OneTimeSetUp]
+        public void SetUpExampleRecipe()
+        {
+            recipe = new Recipe();
+
+            recipe.AddIngredient("Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8");
+            recipe.AddIngredient("Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3");
+        }
+
+        [Test]
+        public void GetCookieScoreForExample()
+        {
+            var ratios = new Dictionary<string, int>();
+            ratios.Add("Butterscotch", 44);
+            ratios.Add("Cinnamon", 56);
+
+            var actual = recipe.GetCookieScore(ratios);
+            var expected = 62842880;
+
+            Assert.AreEqual(expected, actual);
         }
     }
 }
--- AdventOfCode.Day15/Program.cs
+++ AdventOfCode.Day15/Program.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using TCL.Extensions;
 
 namespace AdventOfCode.Day15
 {
@@ -10,6 +12,83 @@ namespace AdventOfCode.Day15
     {
         static void Main(string[] args)
         {
+
+        }
+    }
+
+    public class Recipe
+    {
+        private List<Ingredient> ingredients = new List<Ingredient>();
+
+        public void AddIngredient(string rawIngredientListing)
+        {
+            ingredients.Add(new Ingredient(rawIngredientListing));
+        }
+
+        /// <summary>
+        /// Determines the total score of a cookie with the given ratios.
+        /// </summary>
+        /// <param name="ingredientRatios">Key value is the name of the ingredient. Value is the 0-100 ratio of the cookie.</param>
+        /// <returns></returns>
+        public int GetCookieScore(Dictionary<string, int> ingredientRatios)
+        {
+            var summedIngredientScores = ingredientRatios
+                .Join(ingredients,
+                      (x) => x.Key,
+                      (y) => y.Name,
+                      (x, y) => new { Ingredient = y, Ratio = x.Value })
+                .Select(x => new
+                {
+                    Capacity = x.Ingredient.Capacity * x.Ratio,
+                    Durrability = x.Ingredient.Durrability * x.Ratio,
+                    Flavor = x.Ingredient.Flavor * x.Ratio,
+                    Texture = x.Ingredient.Texture * x.Ratio
+                })
+                .Aggregate((x, y) => new
+                {
+                    Capacity = x.Capacity + y.Capacity,
+                    Durrability = x.Durrability + y.Durrability,
+                    Flavor = x.Flavor + y.Flavor,
+                    Texture = x.Texture + y.Texture
+                });
+
+            // if any of the properties is 0 or lower, the result of the multiplication
+            // will be 0, so just return now
+            if (summedIngredientScores.Capacity <= 0) return 0;
+            if (summedIngredientScores.Durrability <= 0) return 0;
+            if (summedIngredientScores.Flavor <= 0) return 0;
+            if (summedIngredientScores.Texture <= 0) return 0;
+
+            var totalCookieScore =
+                summedIngredientScores.Capacity *
+                summedIngredientScores.Durrability *
+                summedIngredientScores.Flavor *
+                summedIngredientScores.Texture;
+
+            return totalCookieScore;
+        }
+    }
+
+    public class Ingredient
+    {
+        public string Name { get; set; }
+
+        public int Capacity { get; set; }
+        public int Durrability { get; set; }
+        public int Flavor { get; set; }
+        public int Texture { get; set; }
+        public int Calories { get; set; }
+
+        public Ingredient(string rawIngredientListing)
+        {
+            var match = Regex.Match(rawIngredientListing, @"(\w+): \w+ ([-\d]+), \w+ ([-\d]+), \w+ ([-\d]+), \w+ ([-\d]+), \w+ ([-\d]+)");
+
+            Name = match.Groups[1].Value;
+            Capacity = match.Groups[2].Value.Parse<int>();
+            Durrability = match.Groups[3].Value.Parse<int>();
+            Flavor = match.Groups[4].Value.Parse<int>();
+            Texture = match.Groups[5].Value.Parse<int>();
+            Calories = match.Groups[6].Value.Parse<int>();
         }
     }
 }
