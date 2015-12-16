f1d143e1a86699274d15b07dd5000ceb51a43736
Kevin Hunter <kevin.hunter@sylvania.com>
2015-12-16 08:43:41 -0500

    empty day 16 code

---
+++ AdventOfCode.Day16.Tests/Day16Tests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace AdventOfCode.Day16.Tests
+{
+    [TestFixture]
+    public class Day16Tests
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
