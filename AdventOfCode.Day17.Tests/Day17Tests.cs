0d237baf3b08f2d991d7f64af38272f2806f9a6e
Kevin Hunter <kevin.hunter@sylvania.com>
2015-12-17 09:16:54 -0500

    part 1 example works

--- AdventOfCode.Day17.Tests/Day17Tests.cs
+++ AdventOfCode.Day17.Tests/Day17Tests.cs
@@ -6,9 +6,27 @@ namespace AdventOfCode.Day17.Tests
     [TestFixture]
     public class Day17Tests
     {
-        [Test]
-        public void TestMethod1()
+        private Storage storage;
+
+        [OneTimeSetUp]
+        public void SetupExample()
         {
+            storage = new Storage();
+
+            storage.AddContainer(20);
+            storage.AddContainer(15);
+            storage.AddContainer(10);
+            storage.AddContainer(5);
+            storage.AddContainer(5);
+        }
+
+        [Test]
+        public void GetContainerCombinationCountForExample()
+        {
+            var totalLiquidSize = 25;
+            var expectedComboCount = 4;
+            var actual = storage.GetContainerCombinationCount(totalLiquidSize);
+            Assert.AreEqual(expectedComboCount, actual);
         }
     }
 }
--- AdventOfCode.Day17/Program.cs
+++ AdventOfCode.Day17/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AdventOfCode.Common;
+using TCL.Extensions;
 
 namespace AdventOfCode.Day17
 {
@@ -13,4 +15,68 @@ namespace AdventOfCode.Day17
 
         }
     }
+
+    public class Storage
+    {
+        private List<Container> containers = new List<Container>();
+
+        public void AddContainer(int containerSize)
+        {
+            containers.Add(new Container() { Capacity = containerSize });
+        }
+
+        public int GetContainerCombinationCount(int totalLiquidSize)
+        {
+            var workingCombinations = EnumCombinationsOfAllLenghts()
+                .Where(x => x.Sum(c => c.Capacity) == totalLiquidSize);
+
+            var comboCount = workingCombinations.Count();
+
+            return comboCount;
+        }
+
+        private IEnumerable<IEnumerable<Container>> EnumCombinationsOfAllLenghts()
+        {
+            for (int i = 1; i < containers.Count; i++)
+            {
+                foreach (var combo in containers.GetKCombs(i))
+                {
+                    yield return combo;
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// This entire class exists because you can have multiple containers
+    /// of the same capacity, but are identified differently.
+    /// </summary>
+    public class Container : IComparable, IComparable<Container>
+    {
+        private Guid identifier;
+
+        public int Capacity { get; set; }
+
+        public Container()
+        {
+            identifier = Guid.NewGuid();
+        }
+
+        public int CompareTo(object obj)
+        {
+            return CompareTo((Container)obj);
+        }
+
+        public int CompareTo(Container other)
+        {
+            return MakeComparingString().CompareTo(other.MakeComparingString());
+        }
+
+        private string MakeComparingString()
+        {
+            return "{0}:{1}".FormatInline(Capacity, identifier.ToString());
+        }
+
+    }
+
 }
