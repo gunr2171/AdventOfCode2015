0fc80e36863a522c9df6ed8316fd8a618cd78f90
Kevin Hunter <kevin.hunter@sylvania.com>
2015-12-18 15:46:12 -0500

    almost done with the tests for part 1

--- AdventOfCode.Day18.Tests/Day18Tests.cs
+++ AdventOfCode.Day18.Tests/Day18Tests.cs
@@ -48,10 +48,38 @@ namespace AdventOfCode.Day18.Tests
         [TestCase(Example_Initial, 4, Example_4)]
         public void RunBoardAnimation(string initialLayout, int stepCount, string expectedOutput)
         {
-            var startingBoard = new Board(initialLayout);
+            var startingBoard = new Board(6);
+            startingBoard.SetBoardValues(initialLayout);
             var actual = Processor.RunBoardAnimation(startingBoard, stepCount);
 
             Assert.AreEqual(expectedOutput, actual.ToString());
+        }
+
+        [TestCase(true, 2, true)]
+        [TestCase(true, 3, true)]
+        [TestCase(true, 4, false)]
+        [TestCase(true, 1, false)]
+        [TestCase(false, 3, true)]
+        [TestCase(false, 1, false)]
+        public void CalculateNewValueForLight(bool currentValue, int litNeighborCount, bool expectedNewValue)
+        {
+            var actual = Processor.CalculateNewValueForLight(currentValue, litNeighborCount);
+            Assert.AreEqual(expectedNewValue, actual);
+        }
+
+        [TestCase(Example_3, 0, 3, 0)]
+        [TestCase(Example_3, 1, 3, 2)]
+        [TestCase(Example_2, 5, 0, 2)]
+        [TestCase(Example_2, 5, 5, 0)]
+        [TestCase(Example_4, 2, 2, 3)]
+        [TestCase(Example_1, 1, 4, 5)]
+        [TestCase(Example_Initial, 5, 5, 1)]
+        public void CalculateLitNeighborsForLight(string gridLayout, int xIndex, int yIndex, int expectedLitNeighbors)
+        {
+            var board = new Board(6);
+            board.SetBoardValues(gridLayout);
+            var actual = board.CalculateLitNeighborsForLight(xIndex, yIndex);
+            Assert.AreEqual(expectedLitNeighbors, actual);
         }
     }
 }
--- AdventOfCode.Day18/Program.cs
+++ AdventOfCode.Day18/Program.cs
@@ -18,16 +18,144 @@ namespace AdventOfCode.Day18
     {
         public static Board RunBoardAnimation(Board initialBoardLayout, int stepCount)
         {
-            throw new NotImplementedException();
+            var steps = Enumerable.Range(1, stepCount);
+            var workingBoard = initialBoardLayout;
+
+            foreach (var step in steps)
+            {
+                workingBoard = workingBoard.StepBoard();
+            }
+
+            return workingBoard;
+        }
+
+        public static bool CalculateNewValueForLight(bool currentValue, int litNeighborCount)
+        {
+            if (currentValue == true && (litNeighborCount == 2 || litNeighborCount == 3))
+            {
+                return true;
+            }
+
+            if (currentValue == false && litNeighborCount == 3)
+            {
+                return true;
+            }
+
+            //else, turn off
+            return false;
         }
     }
 
     public class Board
     {
+        private int gridSize;
 
-        public static Board FromInstructionString(string matrix)
+        public Board(int side)
         {
-            throw new NotImplementedException();
+            lights = new bool[side, side];
+            gridSize = side;
+        }
+
+        private bool[,] lights;
+
+        public void SetBoardValues(string rawMatrix)
+        {
+            var rows = rawMatrix
+                .Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+            for (int row = 0; row < gridSize; row++)
+            {
+                for (int column = 0; column < gridSize; column++)
+                {
+                    lights[row, column] = rows[row][column] == '#';
+                }
+            }
+        }
+
+        /// <summary>
+        /// Returns a new board based on this board but animated by one step.
+        /// </summary>
+        /// <returns></returns>
+        public Board StepBoard()
+        {
+            var newBoard = new Board(gridSize);
+            Array.Copy(this.lights, newBoard.lights, gridSize); //copy over the light values
+
+            for (int row = 0; row < gridSize; row++)
+            {
+                for (int column = 0; column < gridSize; column++)
+                {
+                    var litNeighborCount = CalculateLitNeighborsForLight(row, column);
+                    var currentValue = lights[row, column];
+                    var newValue = Processor.CalculateNewValueForLight(currentValue, litNeighborCount);
+
+                    newBoard.lights[row, column] = newValue;
+                }
+            }
+
+            return newBoard;
+        }
+
+        public int CalculateLitNeighborsForLight(int xIndex, int yIndex)
+        {
+            var allLights = from row in Enumerable.Range(0, gridSize)
+                            from column in Enumerable.Range(0, gridSize)
+                            select new { X = row, Y = column, Value = lights[row, column] };
+
+            var neighbors = allLights
+                .Where(l =>
+                    (l.X == xIndex - 1 && l.Y == yIndex - 1) || //top left
+                    (l.X == xIndex - 1 && l.Y == yIndex + 0) || //top
+                    (l.X == xIndex - 1 && l.Y == yIndex + 1) || //top right
+                    (l.X == xIndex + 0 && l.Y == yIndex + 1) || //right
+                    (l.X == xIndex + 1 && l.Y == yIndex + 1) || //bottom right
+                    (l.X == xIndex + 1 && l.Y == yIndex + 0) || //bottom
+                    (l.X == xIndex + 1 && l.Y == yIndex - 1) || //bottom left
+                    (l.X == xIndex + 0 && l.Y == yIndex - 1));  //left
+
+            var litNeighbors = neighbors.Where(x => x.Value == true);
+            var litNeighborCount = litNeighbors.Count();
+
+            return litNeighborCount;
+        }
+
+        public override string ToString()
+        {
+            var builder = new StringBuilder();
+
+            for (int row = 0; row < gridSize; row++)
+            {
+                for (int column = 0; column < gridSize; column++)
+                {
+                    var cellDisplayValue = lights[row, column]
+                        ? "#"
+                        : ".";
+
+                    builder.Append(cellDisplayValue);
+                }
+
+                builder.AppendLine();
+            }
+
+            return builder.ToString();
+        }
+    }
+
+    public static class Extensions
+    {
+        public static IEnumerable<IEnumerable<T>> EnumValues<T>(T[,] source, int size)
+        {
+            var values = from x in Enumerable.Range(0, size)
+                         from y in Enumerable.Range(0, size)
+                         orderby x
+                         orderby y
+                         select new { X = x, Y = y, Value = source[x, y] };
+
+            var result = values
+                .GroupBy(x => x.X)
+                .Select(x => x.Select(y => y.Value));
+
+            return result;
         }
     }
 }
