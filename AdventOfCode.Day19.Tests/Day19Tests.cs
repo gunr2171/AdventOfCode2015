e0637f3aba620d2f94774a11f40dd4de16e110af
gunr2171 <kevin@kevin-hunter.net>
2015-12-19 11:00:17 -0500

    empty day 19 code

---
+++ AdventOfCode.Day19.Tests/Day19Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace AdventOfCode.Day19.Tests
+{
+    [TestFixture]
+    public class Day19Tests
+    {
+        [Test]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
