ae1142d0c7aa4d907e6109c8b4325ca87965ffbb
gunr2171 <kevin@kevin-hunter.net>
2015-12-19 13:03:17 -0500

    going to make a checkpoint here I'm going to try out regex instead

--- AdventOfCode.Day19.Tests/Day19Tests.cs
+++ AdventOfCode.Day19.Tests/Day19Tests.cs
@@ -6,10 +6,18 @@ namespace AdventOfCode.Day19.Tests
     [TestFixture]
     public class Day19Tests
     {
-        [Test]
-        public void TestMethod1()
+        [TestCase("HOH", 4)]
+        [TestCase("HOHOHO", 7)]
+        public void Part1Example(string input, int expectedDistinctMolecules)
         {
+            var machine = new Machine();
 
+            machine.AddReplacement("H => HO");
+            machine.AddReplacement("H => OH");
+            machine.AddReplacement("O => HH");
+
+            var actual = machine.CalculateDistinctMoleculesCount(input);
+            Assert.AreEqual(expectedDistinctMolecules, actual);
         }
     }
 }
--- AdventOfCode.Day19/Program.cs
+++ AdventOfCode.Day19/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace AdventOfCode.Day19
@@ -13,4 +14,58 @@ namespace AdventOfCode.Day19
 
         }
     }
+
+    public class Machine
+    {
+        private List<Replacement> replacements = new List<Replacement>();
+
+        public void AddReplacement(string rawInstruction)
+        {
+            var match = Regex.Match(rawInstruction, @"(\w+) => (\w+)");
+
+            var newReplacement = new Replacement();
+            newReplacement.Input = match.Groups[1].Value;
+            newReplacement.Output = match.Groups[2].Value;
+
+            replacements.Add(newReplacement);
+        }
+
+        public int CalculateDistinctMoleculesCount(string inputMolecule)
+        {
+            var startingMolecule = new Molecule(inputMolecule);
+
+            foreach (var atom in startingMolecule.EnumAtoms())
+            {
+                var matchingReplacements = replacements
+                    .Where(x => x.Input == atom);
+
+                foreach (var replacement in matchingReplacements)
+                {
+                    
+                }
+            }
+        }
+    }
+
+    public class Replacement
+    {
+        public string Input { get; set; }
+        public string Output { get; set; }
+    }
+
+    public class Molecule
+    {
+        private List<char> letters = new List<char>();
+
+        public Molecule(string input)
+        {
+
+        }
+
+        public IEnumerable<string> EnumAtoms()
+        {
+
+        }
+    }
+
 }
