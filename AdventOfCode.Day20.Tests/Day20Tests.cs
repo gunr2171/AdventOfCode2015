2d35475c4ea78e889f2cecb6174c7d0a9c37cc3c
Kevin Hunter <kevin.hunter@sylvania.com>
2015-12-21 09:50:05 -0500

    "done" with day 20 this one I didn't get on my own, had to look up online. the unit tests don't work right now and later I'll go and make those things work

--- AdventOfCode.Day20.Tests/Day20Tests.cs
+++ AdventOfCode.Day20.Tests/Day20Tests.cs
@@ -6,10 +6,19 @@ namespace AdventOfCode.Day20.Tests
     [TestFixture]
     public class Day20Tests
     {
-        [Test]
-        public void TestMethod1()
+        [TestCase(1, 10)]
+        [TestCase(2, 30)]
+        [TestCase(3, 40)]
+        [TestCase(4, 70)]
+        [TestCase(5, 60)]
+        [TestCase(6, 120)]
+        [TestCase(7, 80)]
+        [TestCase(8, 150)]
+        [TestCase(9, 130)]
+        public void GetPresentCountForHouseNumber(int houseNumber, int expectedPresentCount)
         {
-
+            var actual = Processor.GetPresentCountForHouseNumber(houseNumber);
+            Assert.AreEqual(expectedPresentCount, actual);
         }
     }
 }
--- AdventOfCode.Day20/Program.cs
+++ AdventOfCode.Day20/Program.cs
@@ -10,6 +10,52 @@ namespace AdventOfCode.Day20
     {
         static void Main(string[] args)
         {
+            var targetNumber = 36000000;
+
+            var part1Answer = Processor.Run(targetNumber);
+        }
+    }
+
+    public static class Processor
+    {
+        public static int Run(int targetPresentCount)
+        {
+            var houseValues = new Dictionary<int, int>();
+
+            for (int i = 1; i <= targetPresentCount / 10; i++)
+            {
+                for (int j = i; j <= targetPresentCount / 10; j += i)
+                {
+                    if (!houseValues.ContainsKey(j))
+                    {
+                        houseValues.Add(j, i * 10);
+                    }
+                    else
+                    {
+                        houseValues[j] += i * 10;
+                    }
+                }
+            }
+
+            var smallestHouseWithTargetValue = houseValues
+                .Where(x => x.Value >= targetPresentCount)
+                .Select(x => x.Key)
+                .Min();
+
+            return smallestHouseWithTargetValue;
+        }
+
+
+        public static int GetPresentCountForHouseNumber(int houseNumber)
+        {
+            var presentCount = 0;
+
+            for (int i = 1; i <= houseNumber; i += houseNumber)
+            {
+                presentCount += i * 10;
+            }
+
+            return presentCount;
         }
     }
 }
