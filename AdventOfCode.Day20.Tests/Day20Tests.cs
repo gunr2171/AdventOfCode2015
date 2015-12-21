db1f95aae9c4e40b094cec4a2ff72e103d741890
Kevin Hunter <kevin.hunter@sylvania.com>
2015-12-21 09:21:58 -0500

    empty day 20 code

---
+++ AdventOfCode.Day20.Tests/Day20Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace AdventOfCode.Day20.Tests
+{
+    [TestFixture]
+    public class Day20Tests
+    {
+        [Test]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
