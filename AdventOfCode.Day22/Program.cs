b541a2c848df193f13a3432b3f9ce9f19d594909
Kevin Hunter <kevin.hunter@sylvania.com>
2015-12-22 09:46:17 -0500

    good chuck of the setup code is done for part 1

--- AdventOfCode.Day22/Program.cs
+++ AdventOfCode.Day22/Program.cs
@@ -12,4 +12,131 @@ namespace AdventOfCode.Day22
         {
         }
     }
+
+    public static class Processor
+    {
+        public static bool SimulateFight(FighterStats initialPlayerStats, BossStats initialBossStats, List<Spell> playerSpellList)
+        {
+            throw new NotImplementedException();
+
+            var activeEffects = new List<Effect>();
+            bool playerIsAttacking = true;
+            var currentSpellIndex = 0;
+
+            var currentFightStats = new FightRoundResults() { PlayerStats = new FighterStats(initialPlayerStats), BossStats = new BossStats(initialBossStats) };
+
+            while (currentFightStats.PlayerStats.HitPoints > 0 && currentFightStats.BossStats.HitPoints > 0)
+            {
+                //if it's the player's turn, check if you ran out of spells
+                if (playerIsAttacking && currentSpellIndex == playerSpellList.Count)
+                {
+                    //player can't cast a spell, dies
+                    currentFightStats.PlayerStats.HitPoints = -1;
+                    break;
+                }
+
+                SimulateRound(activeEffects, playerIsAttacking, currentFightStats, playerSpellList[currentSpellIndex]);
+                currentSpellIndex += 1;
+            }
+        }
+
+        private static void SimulateRound(List<Effect> activeEffects, bool playerIsAttacking, FightRoundResults currentStats, Spell spellToCast)
+        {
+            //start of round, apply effects
+            foreach (var effect in activeEffects)
+            {
+                throw new NotImplementedException();
+            }
+
+            if (playerIsAttacking)
+            {
+                //can you cast the spell?
+                if (spellToCast.ManaCost > currentStats.PlayerStats.Mana)
+                {
+                    //you don't have enough to cast the spell, die
+                    currentStats.PlayerStats.HitPoints = -1;
+                    return;
+                }
+
+                //cast the spell
+                currentStats.PlayerStats.Mana -= spellToCast.ManaCost;
+                currentStats.PlayerStats.HitPoints += spellToCast.Healing;
+                currentStats.BossStats.HitPoints = CalculateNewHitPointsAfterAttack(currentStats.BossStats.HitPoints, spellToCast.Damage, 0);
+            }
+            else
+            {
+                currentStats.PlayerStats.HitPoints = CalculateNewHitPointsAfterAttack(currentStats.PlayerStats.HitPoints, currentStats.BossStats.Attack, currentStats.PlayerStats.Armor);
+            }
+        }
+
+        public static int CalculateNewHitPointsAfterAttack(int startingHitPoints, int attackerAttackValue, int defenderArmorValue)
+        {
+            var netDamage = attackerAttackValue - defenderArmorValue;
+
+            if (netDamage <= 0)
+                netDamage = 1;
+
+            var newHP = startingHitPoints - netDamage;
+            return newHP;
+        }
+    }
+
+    public class FighterStats
+    {
+        public int HitPoints { get; set; }
+        public int Armor { get; set; }
+        public int Mana { get; set; }
+
+        public FighterStats() { }
+
+        public FighterStats(FighterStats copyStats)
+        {
+            HitPoints = copyStats.HitPoints;
+            Armor = copyStats.Armor;
+            Mana = copyStats.Mana;
+        }
+
+        public FighterStats(int hp, int armor, int mana)
+        {
+            HitPoints = hp;
+            Armor = armor;
+            Mana = mana;
+        }
+    }
+
+    public class BossStats : FighterStats
+    {
+        public int Attack { get; set; }
+
+        public BossStats(BossStats copyStats):base(copyStats)
+        {
+            Attack = copyStats.Attack;
+        }
+
+        public BossStats(int hp, int armor, int mana, int attack)
+        {
+            Attack = attack;
+        }
+    }
+    
+    public class FightRoundResults
+    {
+        public FighterStats PlayerStats { get; set; }
+        public BossStats BossStats { get; set; }
+    }
+
+    public class Spell
+    {
+        public string Name { get; set; }
+        public int ManaCost { get; set; }
+        public int Damage { get; set; }
+        public int Healing { get; set; }
+
+        public Effect Effect { get; set; }
+    }
+
+    public class Effect
+    {
+        //public int T
+    }
 }
