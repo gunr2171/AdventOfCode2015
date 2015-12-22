99d6534a5130833086ff56f991d3ff8fc6a3188d
Kevin Hunter <kevin.hunter@sylvania.com>
2015-12-22 09:09:18 -0500

    empty day 22 code

---
+++ AdventOfCode.Day22.Tests/Day22Tests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace AdventOfCode.Day22.Tests
+{
+    [TestFixture]
+    public class Day22Tests
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
